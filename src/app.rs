eff497e061a3bc5505fa3906ea9c2ce76b0cd7a5
Victor Rodriguez <awtgerry@gmail.com>
2023-11-22 14:06:10 -0600

    timer return

--- src/app.rs
+++ src/app.rs
@@ -2,9 +2,10 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
-use crate::timer::Timer;
+// use crate::timer::Timer;
 
 use getrandom::getrandom;
+use std::time::Duration;
 
 #[allow(unused)]
 const WORDS: &str = include_str!("../dictionary/English.txt");
@@ -115,3 +116,61 @@ fn get_random_words(amount: u16) -> String {
     result
 }
 
+#[component]
+pub fn Timer(cx: Scope, signal: RwSignal<bool>, timer: RwSignal<usize>)
+    -> impl IntoView
+{
+    return view! {cx,
+        <div>
+        {move || {
+            if signal.get() {
+                use_interval(cx, 1000, move || {
+                    /* TODO: Deal with this later */
+                    /* It shoud save the wpm of the user in the db */
+                    if timer.get() <= 0 {
+                        timer.set(30);
+                        signal.set(false);
+                        // set_userinput("".to_string());
+                    }
+                    timer.update(|c| *c = *c - 1);
+                });
+                view! {cx,
+                    <div class="flex">
+                        <img src="/clock.svg" class="w-12 h-12"/>
+                        <h1 class="text-aw-green-light ml-2 text-4xl text-bold font-pacifico self-stretch">{timer}</h1>
+                    </div>
+                }
+            } else {
+                view! {cx,
+                    <div class="flex">
+                        <img src="/clock.svg" class="w-12 h-12"/>
+                        <h1 class="text-aw-green-light ml-2 text-4xl text-bold font-pacifico self-stretch">{timer}</h1>
+                    </div>
+                }
+            }
+        }}
+        </div>
+    }
+}
+
+#[allow(unused)]
+fn use_interval<T, F>(cx: Scope, interval_millis: T, f: F)
+where
+    F: Fn() + Clone + 'static,
+    T: Into<MaybeSignal<u64>> + 'static,
+{
+    let interval_millis = interval_millis.into();
+    create_effect(cx, move |prev_handle: Option<IntervalHandle>| {
+        if let Some(prev_handle) = prev_handle {
+            prev_handle.clear();
+        };
+
+        set_interval(
+            f.clone(),
+            // this is the only reactive access, so this effect will only
+            // re-run when the interval changes
+            Duration::from_millis(interval_millis.get()),
+        )
+        .expect("could not create interval")
+    });
+}
